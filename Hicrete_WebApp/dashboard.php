bc2388b343cff7ff1a2eb25f42f0d7566b94d0e8
DELL <kharat212@gmail.com>
2016-03-06 19:53:01 +0530

    Fixed the top header issues for Dashboard.php.

--- Hicrete_WebApp/dashboard.php
+++ Hicrete_WebApp/dashboard.php
@@ -245,13 +245,22 @@ if(!$userObj->init($userId)){
             <li class="xn-icon-button">
                 <a href="#" class="x-navigation-minimize"><span class="fa fa-dedent"></span></a>
             </li>
+            <li class="xn-icon-button">
+                <a href="#" title="Bill Approval"><span class="fa fa-file-text-o"></span></a>
+            </li>
+            <li class="xn-icon-button">
+                <a href="#" title="Access Approval"><span class="fa fa-check-square-o"></span></a>
+            </li>
+            <li class="xn-icon-button">
+                <a href="#" title="Leave Approval"><span class="fa fa-thumbs-o-up"></span></a>
+            </li>
             <!-- END TOGGLE NAVIGATION -->
             <!-- SEARCH -->
-            <li class="xn-search">
-                <form role="form">
-                    <input type="text" name="search" placeholder="Search..."/>
-                </form>
-            </li>
+<!--            <li class="xn-search">-->
+<!--                <form role="form">-->
+<!--                    <input type="text" name="search" placeholder="Search..."/>-->
+<!--                </form>-->
+<!--            </li>-->
             <!-- END SEARCH -->
             <!-- SIGN OUT -->
             <!--            <li class="xn-icon-button pull-right">-->
@@ -284,34 +293,17 @@ if(!$userObj->init($userId)){
             <!-- END SIGN OUT -->
             <!-- MESSAGES -->
             <li class="xn-icon-button pull-right">
-                <a href="#"><span class="fa fa-comments"></span></a>
+                <a href="#"><span class="fa fa-bell"></span></a>
                 <div class="informer informer-danger">4</div>
                 <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging">
                     <div class="panel-heading">
-                        <h3 class="panel-title"><span class="fa fa-comments"></span> Messages</h3>
+                        <h3 class="panel-title"><span class="fa fa-comments"></span>Notifications</h3>
                         <div class="pull-right">
                             <span class="label label-danger">4 new</span>
                         </div>
                     </div>
                 </div>
             </li>
-
-
-            <li class="xn-icon-button pull-right">
-                <a href="#"><span class="fa fa-tasks"></span></a>
-                <div class="informer informer-warning">3</div>
-                <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging">
-                    <div class="panel-heading">
-                        <h3 class="panel-title"><span class="fa fa-tasks"></span> Tasks</h3>
-                        <div class="pull-right">
-                            <span class="label label-warning">3 active</span>
-                        </div>
-                    </div>
-                 </div>
-            </li>
-
-
-
                     <!-- END SIGN OUT -->
 
 
