b125b529e906cda8453bb935c810a5cfda02f178
Pranav <pranavkulkarni1692@gmail.com>
2016-07-03 20:51:11 +0530

    Raw material condition while fetching material for Production batch

--- Hicrete_WebApp/Inventory/php/fetch_materials.php
+++ Hicrete_WebApp/Inventory/php/fetch_materials.php
@@ -1,19 +1,25 @@
 <?php
 
-$hostname = 'localhost';
-$dbname='hicrete';
-$username = 'hicreteRoot';
-$password = 'hicrete@123';
-$userId="Pranav";
+//$hostname = 'localhost';
+//$dbname='hicrete';
+//$username = 'hicreteRoot';
+//$password = 'hicrete@123';
+//$userId="Pranav";
 
-$dbh= new PDO("mysql:host=$hostname;dbname=$dbname" , $username ,$password);
+require_once 'Database/Database.php';
+$db = Database::getInstance();
+$dbh = $db->getConnection();
+//$dbh= new PDO("mysql:host=$hostname;dbname=$dbname" , $username ,$password);
 $opt = array(
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 );
 
+$materialType="HICRETE RAW MATERIAL";
+$stmt = $dbh->prepare("select a.materialid,b.productname from material a,product_master b where a.productmasterid=b.productmasterid
+                      AND b.materialtypeid=(SELECT materialtypeid FROM materialtype WHERE UPPER(materialtype)=UPPER(:materialtype))");
+$stmt->bindParam(':materialtype',$materialType);
 
-$stmt = $dbh->prepare("select a.materialid,b.productname from material a,product_master b where a.productmasterid=b.productmasterid"); 
 $stmt->execute();
 $result=$stmt->fetchAll();
 
